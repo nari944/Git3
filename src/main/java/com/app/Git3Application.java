b39c528712e4b158778149ed517720112dad8292
sk <sk@192.168.1.132>
2021-02-04 15:28:21 +0530

    1st commit

---
+++ src/main/java/com/app/Git3Application.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git3Application.class, args);
+		
+		System.out.println("Gittttttttttttttttttttttt");
+	}
+
+}
